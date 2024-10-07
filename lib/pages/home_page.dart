d986167aa68d57698d1d92982c2fdb73d6b5748c
Benjamin <binemugha@gmail.com>
2024-10-07 08:46:02 +0100

    Add not but not fully working

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:simple_notes_app/services/firestore.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final FirestoreService firestoreService = FirestoreService();
+  final TextEditingController textController = TextEditingController();
+
+  void openNoteBox() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              firestoreService.addNote(textController.text);
+              textController.clear();
+              Navigator.pop(context);
+            },
+            child: const Text('Add'),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Notes')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: openNoteBox,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
